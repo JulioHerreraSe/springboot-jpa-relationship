dd3abb57546a4f8421fdfbe698f869586ce92abf
JulioHerreraSe <julio.herreraseara@gmail.com>
2024-10-29 11:11:19 +0100

    First commit

---
+++ src/main/java/com/julio/curso/springboot/jpa/springboot_jpa_relationship/SpringbootJpaRelationshipApplication.java
@@ -0,0 +1,57 @@
+package com.julio.curso.springboot.jpa.springboot_jpa_relationship;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.julio.curso.springboot.jpa.springboot_jpa_relationship.entities.Client;
+import com.julio.curso.springboot.jpa.springboot_jpa_relationship.entities.Invoice;
+import com.julio.curso.springboot.jpa.springboot_jpa_relationship.repositories.ClientRepository;
+import com.julio.curso.springboot.jpa.springboot_jpa_relationship.repositories.InvoiceRepository;
+
+@SpringBootApplication
+public class SpringbootJpaRelationshipApplication implements CommandLineRunner{
+
+	@Autowired
+	private ClientRepository clientRepository;
+
+	@Autowired
+	private InvoiceRepository invoiceRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaRelationshipApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+		manyToOneFindByIdClient();
+	}
+
+	public void manyToOne() {
+
+		Client client = new Client("Jhon", "Doe");
+		clientRepository.save(client);
+
+		Invoice invoice = new Invoice("compras de oficina", 2000L);
+		invoice.setClient(client);
+		Invoice invoiceDB = invoiceRepository.save(invoice);
+		System.out.println(invoiceDB);
+	}
+
+	public void manyToOneFindByIdClient() {
+
+		Optional<Client> optionalClient = clientRepository.findById(1L);
+
+		if (optionalClient.isPresent()) {
+			Client client = optionalClient.orElseThrow();
+			Invoice invoice = new Invoice("compras de oficina", 2000L);
+			invoice.setClient(client);
+			Invoice invoiceDB = invoiceRepository.save(invoice);
+			System.out.println(invoiceDB);
+		}
+
+	}
+
+}
